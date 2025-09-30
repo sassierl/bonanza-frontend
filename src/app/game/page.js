79d96a241ed0fa4b7ac323a95478d298af1fa27e
sassierl <149876150+sassierl@users.noreply.github.com>
2025-09-30 22:06:02 +0200

    First pages

---
+++ src/app/game/page.js
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+
+export default function Page() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Welcome to Thomas</h1>
+    </div>
+  );
+}
